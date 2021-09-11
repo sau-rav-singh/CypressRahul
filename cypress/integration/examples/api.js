cd595f70b7a95143ad268d03cb8f75400bf9f8ef
Saurav Singh <singh.saurav@icloud.com>
2021-09-11 17:43:26 +0530

    modified api

--- cypress/integration/examples/api.js
+++ cypress/integration/examples/api.js
@@ -7,22 +7,41 @@ it('My FirstTest case',function() {
  
     cy.visit("https://rahulshettyacademy.com/angularAppdemo/");
  
-    cy.intercept('GET','https://rahulshettyacademy.com/Library/GetBook.php?AuthorName=shetty',
-    (req)=>
-    {
-    req.url="https://rahulshettyacademy.com/Library/GetBook.php?AuthorName=malhotra"
+    cy.intercept({
+        method : 'GET',
+        url : 'https://rahulshettyacademy.com/Library/GetBook.php?AuthorName=shetty'
+    },
  
-    req.continue((res)=>
-    {
-       // expect(res.statusCode).to.equal(403)
-    })
- }
- ).as("dummyUrl")
+     {
+         statusCode : 200,
+         body : [{
+                "book_name": "RestAssured with Java",
+                "isbn": "RSU",
+                "aisle": "2301"    },
+               ]
+          
+     }).as('bookretrievals')
+     cy.get("button[class='btn btn-primary']").click()
+     cy.wait('@bookretrievals').should(({request,response})=>
+     {
+         cy.get('tr').should('have.length',response.body.length+1)
+      
+     })
+     cy.get('p').should('have.text','Oops only 1 Book available')
  
- cy.get("button[class='btn btn-primary']").click()
- cy.wait('@dummyUrl')
+ 
+ 
+     //length of the response array = rows of the table
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
  
 })
  
 })
- 
